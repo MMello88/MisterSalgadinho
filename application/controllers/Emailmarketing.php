9fe3303cbbb0cf4c8ac27ccb2c0962ea919b97d6
Matheus <matheusnarciso@hotmail.com>
2018-04-02 09:04:48 -0300

    primeiro teste com o tortoise

--- application/controllers/Emailmarketing.php
+++ application/controllers/Emailmarketing.php
@@ -15,23 +15,23 @@ class Emailmarketing extends CI_Controller {
     $this->data['link'] = $link;
     $nome = "Matheus";
     $html = 
-"<!DOCTYPE html>
-<html lang=\"pt-br\">
-  <head>
-  <style>
-    
-  </style>
-  </head>
-  <body> 
-    <h4><b>Olá,  {$nome}.</b></h4>
-    <h1>Nós da <b>Mister</b> Salgadinho</h1>
-    <p>
-    <b>Agradecemos pela preferência.</b> Seu pedido foi recebido com <b>sucesso.</b> <br>
-    Em breve este <b>delicioso salgadinho</b> será produzido e entrege no <b>local, data e hora</b> nos informado.<br>
-    </p>
-    <img src=\"{$link}assets/template/img/boneco_2.png\">
-  </body>
-</html>";
+      "<!DOCTYPE html>
+      <html lang=\"pt-br\">
+        <head>
+        <style>
+          
+        </style>
+        </head>
+        <body> 
+          <h4><b>Olá,  {$nome}.</b></h4>
+          <h1>Nós da <b>Mister</b> Salgadinho</h1>
+          <p>
+          <b>Agradecemos pela preferência.</b> Seu pedido foi recebido com <b>sucesso.</b> <br>
+          Em breve este <b>delicioso salgadinho</b> será produzido e entrege no <b>local, data e hora</b> nos informado.<br>
+          </p>
+          <img src=\"{$link}assets/template/img/boneco_2.png\">
+        </body>
+      </html>";
 
     $this->load->library('email');
     $this->email
